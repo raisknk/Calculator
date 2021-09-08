2a96ab48e0057b9b12d2106ae880c11944f883a2
Oscar G Sialer-Tapia <72403275+raisknk@users.noreply.github.com>
2021-09-07 21:39:43 -0700

    Add files via upload

---
+++ CalculatorTest.java
@@ -0,0 +1,27 @@
+public class CalculatorTest {
+    public static void main(String[] args) {
+        CalculatorTest c = Calculator();
+        c.setNum(10.5);
+        c.setNum(5.2);
+        c.setOperator("+");
+        c.performOperation();
+        c.getResults();
+    }
+
+    private void setOperator(String string) {
+    }
+
+    private void getResults() {
+    }
+
+    private void performOperation() {
+    }
+
+    private void setNum(double d) {
+    }
+
+    private static CalculatorTest Calculator() {
+        return null;
+    }
+    
+}
